4b4a530318e78b8154607a65050624f63eef4ba5
vasanthakumar-a <vasanthakumara117@gmail.com>
2026-04-22 02:06:31 +0530

    implemented flash for turbo_frame_request

--- lib/toastify/controller.rb
+++ lib/toastify/controller.rb
@@ -3,13 +3,13 @@ module Toastify
     extend ActiveSupport::Concern
 
     included do
-      after_action :append_toastify_to_stream
+      after_action :append_toastify_to_response
     end
 
     private
 
-    def append_toastify_to_stream
-      return unless request.format.turbo_stream?
+    def append_toastify_to_response
+      return unless request.format.turbo_stream? || turbo_frame_request?
 
       toast_flashes = flash.reject { |type, _| type.to_s.start_with?("toast_") }
       return unless toast_flashes.any?
@@ -17,12 +17,23 @@ module Toastify
       script_content = helpers.toastify_script_tag
       return if script_content.blank?
 
-      stream_tag = turbo_stream.append("flash-outlet", script_content)
+      if request.format.turbo_stream?
+        payload = turbo_stream.append("flash-outlet", script_content).to_s
 
-      if response.body.is_a?(String)
-        response.body += stream_tag.to_s
+        if response.body.is_a?(String)
+          response.body += payload
+        else
+          response.body = response.body.to_s + payload
+        end
       else
-        response.body = response.body.to_s + stream_tag.to_s
+        body = response.body.to_s
+        frame_closing_tag = "</turbo-frame>"
+
+        if body.include?(frame_closing_tag)
+          response.body = body.sub(frame_closing_tag, "#{script_content}#{frame_closing_tag}")
+        else
+          response.body = body + script_content.to_s
+        end
       end
     end
   end
